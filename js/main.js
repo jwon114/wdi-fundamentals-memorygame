60e86c274f41f1a07b3719cbba9b46ea7e7f8fd0
jwon114 <jthgwong@gmail.com>
2017-10-02 14:25:30 +1100

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,22 +1,23 @@
-console.log("up and running");
-
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-var cardTwo = cards[2];
 
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped queen");
-console.log("User flipped king");
-
-if (cardsInPlay.length === 2) {
+var checkForMatch = function() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("found a match");
+		alert("You found a match!");
 	} else {
-		alert("try again");
+		alert("Sorry, try again");
 	}
 }
 
-// console.log("user flipped " + cardOne) 
+var flipCard = function (cardId) {
+	console.log("User flipped " + cards[cardId]);
+
+	cardsInPlay.push(cards[cardId]);
+
+	if (cardsInPlay.length === 2) {
+		checkForMatch();
+	}
+}
+
+flipCard(0);
+flipCard(2);
