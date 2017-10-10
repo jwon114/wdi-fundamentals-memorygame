3074b561c0ec202597f1365276abb72880b64010
jwon114 <jthgwong@gmail.com>
2017-10-10 18:29:23 +1100

    final js component

--- js/main.js
+++ js/main.js
@@ -27,21 +27,42 @@ var checkForMatch = function() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You found a match!");
 	} else {
-		alert("Sorry, try again");
+		alert("No Match! Sorry, try again");
 	}
 }
 
-var flipCard = function (cardId) {
+var flipCard = function () {
+	var cardId = this.getAttribute('data-id');
 	console.log("User flipped " + cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
-
 	cardsInPlay.push(cards[cardId].rank);
-
+	this.setAttribute('src', cards[cardId].cardImage);
 	if (cardsInPlay.length === 2) {
-		checkForMatch();
+		setTimeout(checkForMatch, 500);
 	}
 }
 
-flipCard(0);
-flipCard(2);
+var createBoard = function () {
+	for (var i = 0; i <= 3; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+}
+
+var resetBoard = function () {
+	for (var i = 0; i <= 3; i++) {
+		var cardElement = document.querySelectorAll("[data-id='"+i+"']")[0];
+		console.log(cardElement);
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.addEventListener('click', flipCard);
+		cardsInPlay = [];
+	}
+}
+
+document.getElementsByClassName("reset-button")[0].addEventListener('click', resetBoard);
+
+createBoard();
